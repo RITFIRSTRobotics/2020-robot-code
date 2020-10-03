ed887e8dc7e6d1080cd7a7ca8b96d01ffe2ac278
Connor Henley <thatging3rkid@users.noreply.github.com>
2020-10-02 21:55:10 -0400

    upload new build system, should auto-generate Makefile for each run

---
+++ robot.cpp
@@ -0,0 +1,13 @@
+/**
+ * robot.cpp
+ *
+ * Main entry point for the 2020-robot-code project
+ *
+ * @author Connor Henley, @thatging3rkid
+ */
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    // temporary
+    printf("Hello world!\n");
+}
---
+++ robot.mk
@@ -0,0 +1,33 @@
+# 2020-robot-code Makefile
+#
+# @author Connor Henley, @thatging3rkid
+
+# include defaults from core
+include core/defaults.mk
+
+# setup directories
+OBJ_DIR=./obj
+TEST_DIR=./test
+TEST_OBJ_DIR=$(TEST_DIR)/obj
+
+# allow GCC to find local includes
+CFLAGS += -iquote.
+
+all: $(OBJ_DIR)/robot
+
+$(OBJ_DIR)/robot.o: robot.cpp
+	@mkdir -p $(OBJ_DIR)
+	$(CXX) $(CPPFLAGS) -c -o $@ $<
+
+$(OBJ_DIR)/robot: $(OBJ_DIR)/robot.o
+	@mkdir -p $(OBJ_DIR)
+	$(CXX) -o $@ $^ $(LD_FLAGS)
+
+.PHONY: run
+run: $(OBJ_DIR)/robot
+	@./$<
+
+### Required recipes
+
+ci-build: all
+ci-test:  ; # do nothing
